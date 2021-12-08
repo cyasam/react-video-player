48ce2253da771a701c56cf5e59dd76468dbc77cf
Cagdas ISIK <cyasam@gmail.com>
2021-12-08 06:17:53 +0300

    Video changed.

--- src/video-data.js
+++ src/video-data.js
@@ -6,14 +6,14 @@ const video = {
   sources: [
     {
       id: 1,
-      url: 'video/video-2.mp4',
+      url: 'video/surfing_with_audio_720p.mp4',
       type: 'video/mp4',
     },
-    /* {
+    {
       id: 2,
       url: 'video/surfing_with_audio_720p.webm',
       type: 'video/webm',
-    }, */
+    },
   ],
   thumb: 'video/surfing_with_audio_thumb.jpg',
   title: 'Surfing With Audio',
