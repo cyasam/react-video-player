e389c13931c2ba5caee77ca499dfb47ae6c4a2b8
Cagdas ISIK <cyasam@gmail.com>
2021-12-09 03:29:21 +0300

    Video url changed.

--- src/video-data.js
+++ src/video-data.js
@@ -5,31 +5,18 @@ const video = [
     sources: [
       {
         id: 1,
-        url: 'video/surfing_with_audio_720p.mp4',
+        url: 'https://www.angrygorilla.us/video/surfing_with_audio_720p.mp4',
         type: 'video/mp4',
       },
       {
         id: 2,
-        url: 'video/surfing_with_audio_720p.webm',
+        url: 'https://www.angrygorilla.us/video/surfing_with_audio_720p.webm',
         type: 'video/webm',
       },
     ],
-    thumb: 'video/surfing_with_audio_thumb.jpg',
+    thumb: 'https://www.angrygorilla.us/video/surfing_with_audio_thumb.jpg',
     title: 'Surfing',
   },
-  /* {
-    width: 1280,
-    height: 720,
-    sources: [
-      {
-        id: 1,
-        url: 'video/video-1.mp4',
-        type: 'video/mp4',
-      },
-    ],
-    thumb: 'video/video-1-thumb.jpg',
-    title: 'Avrasya Anket',
-  }, */
 ];
 
 export default video;
