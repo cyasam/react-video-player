9752b01934524df600a650a44886d99d6b93a681
Cagdas ISIK <cyasam@gmail.com>
2021-12-13 23:25:58 +0300

    Mobile UX improvements

--- src/components/VolumeControl.js
+++ src/components/VolumeControl.js
@@ -4,11 +4,27 @@ import { ReactComponent as VolumeIcon } from '../images/volume.svg';
 import { ReactComponent as MuteIcon } from '../images/mute.svg';
 import './VolumeControl.css';
 
+import { makeClassName } from '../utils';
+
 function VolumeControl({ volume, soundStatus, onVolumeClick, onVolumeChange }) {
   const [draggingBullet, setDraggingBullet] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <div className="volume-control" data-dragging-bullet={draggingBullet}>
-      <button className="button" onClick={onVolumeClick}>
+    <div
+      className={makeClassName({ 'volume-control': true, open: clicked })}
+      data-dragging-bullet={draggingBullet}
+      onClick={() => setClicked(true)}
+      onMouseEnter={() => setClicked(true)}
+      onMouseLeave={() => setClicked(false)}
+    >
+      <button
+        className="button"
+        onClick={(event) => {
+          onVolumeClick(event);
+          setClicked(true);
+        }}
+      >
         {soundStatus === 'muted' ? <MuteIcon /> : <VolumeIcon />}
       </button>
       <div className="volume-progress">
@@ -21,6 +37,7 @@ function VolumeControl({ volume, soundStatus, onVolumeClick, onVolumeChange }) {
             }}
             onDragStop={() => {
               setDraggingBullet(false);
+              setClicked(false);
             }}
           />
         </div>
