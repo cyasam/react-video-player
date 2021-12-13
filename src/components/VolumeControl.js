7b4dc8517328f79e9cea302a026d2cd8bd4111f4
Cagdas ISIK <cyasam@gmail.com>
2021-12-13 23:11:14 +0300

    Volume control css changes.

--- src/components/VolumeControl.js
+++ src/components/VolumeControl.js
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import ProgressBar from './widgets/ProgressBar';
 import { ReactComponent as VolumeIcon } from '../images/volume.svg';
 import { ReactComponent as MuteIcon } from '../images/mute.svg';
 import './VolumeControl.css';
 
 function VolumeControl({ volume, soundStatus, onVolumeClick, onVolumeChange }) {
+  const [draggingBullet, setDraggingBullet] = useState(false);
   return (
-    <div className="volume-control">
+    <div className="volume-control" data-dragging-bullet={draggingBullet}>
       <button className="button" onClick={onVolumeClick}>
         {soundStatus === 'muted' ? <MuteIcon /> : <VolumeIcon />}
       </button>
       <div className="volume-progress">
-        <ProgressBar value={volume} onDrag={onVolumeChange} />
+        <div className="volume-progress-inner">
+          <ProgressBar
+            value={volume}
+            onDrag={(event) => {
+              onVolumeChange(event);
+              setDraggingBullet(true);
+            }}
+            onDragStop={() => {
+              setDraggingBullet(false);
+            }}
+          />
+        </div>
       </div>
     </div>
   );
